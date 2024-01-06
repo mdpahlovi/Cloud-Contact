63129254e4f1233062ee8d22b58b861718836561
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2024-01-06 19:55:14 +0600

    [Fixed] Menu Key

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,21 +1,23 @@
 import { Layout, Menu, MenuProps, Image } from "antd";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
 const items: MenuProps["items"] = [
-    { key: 1, label: <Link to="/">All Contact</Link> },
-    { key: 2, label: <Link to="/add-contact">Add Contact</Link> },
+    { key: "/", label: <Link to="/">All Contact</Link> },
+    { key: "/add-contact", label: <Link to="/add-contact">Add Contact</Link> },
 ];
 
 export default function Main() {
+    const { pathname } = useLocation();
+
     return (
         <Layout>
             <Header style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
                 <Link to="/">
                     <Image width={100} src="/logo.png" />
                 </Link>
-                <Menu mode="horizontal" defaultSelectedKeys={["1"]} items={items} />
+                <Menu mode="horizontal" selectedKeys={[pathname]} items={items} />
             </Header>
             <Content style={{ minHeight: "calc(100vh - 130.4px)", padding: "24px 50px" }}>
                 <Outlet />
